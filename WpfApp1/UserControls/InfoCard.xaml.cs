69c05eb9487891507fb9d5d9f081ce37fa512349
刘俊 <91175625+2912070466@users.noreply.github.com>
2022-08-28 12:56:48 +0800

    Add files via upload

---
+++ WpfApp1/UserControls/InfoCard.xaml.cs
@@ -0,0 +1,77 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace WpfApp1.UserControls
+{
+    public partial class InfoCard : UserControl
+    {
+        public InfoCard()
+        {
+            InitializeComponent();
+        }
+
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(InfoCard));
+
+        public string Desc
+        {
+            get { return (string)GetValue(DescProperty); }
+            set { SetValue(DescProperty, value); }
+        }
+
+        public static readonly DependencyProperty DescProperty = DependencyProperty.Register("Desc", typeof(string), typeof(InfoCard));
+
+        public string Percentage
+        {
+            get { return (string)GetValue(PercentageProperty); }
+            set { SetValue(PercentageProperty, value); }
+        }
+
+        public static readonly DependencyProperty PercentageProperty = DependencyProperty.Register("Percentage", typeof(string), typeof(InfoCard));
+
+        public string BottomText
+        {
+            get { return (string)GetValue(BottomTextProperty); }
+            set { SetValue(BottomTextProperty, value); }
+        }
+
+        public static readonly DependencyProperty BottomTextProperty = DependencyProperty.Register("BottomText", typeof(string), typeof(InfoCard));
+        public string Days
+        {
+            get { return (string)GetValue(DaysProperty); }
+            set { SetValue(DaysProperty, value); }
+        }
+
+        public static readonly DependencyProperty DaysProperty = DependencyProperty.Register("Days", typeof(string), typeof(InfoCard));
+
+        public int Value
+        {
+            get { return (int)GetValue(ValueProperty); }
+            set { SetValue(ValueProperty, value); }
+        }
+
+        public static readonly DependencyProperty ValueProperty = DependencyProperty.Register("Value", typeof(int), typeof(InfoCard));
+
+        public bool IsActive
+        {
+            get { return (bool)GetValue(IsActiveProperty); }
+            set { SetValue(IsActiveProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsActiveProperty = DependencyProperty.Register("IsActive", typeof(bool), typeof(InfoCard));
+
+        public MahApps.Metro.IconPacks.PackIconMaterialKind Icon
+        {
+            get { return (MahApps.Metro.IconPacks.PackIconMaterialKind)GetValue(IconProperty); }
+            set { SetValue(IconProperty, value); }
+        }
+
+        public static readonly DependencyProperty IconProperty = DependencyProperty.Register("Icon", typeof(MahApps.Metro.IconPacks.PackIconMaterialKind), typeof(InfoCard));
+
+    }
+}
---
+++ WpfApp1/UserControls/UserCard.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+
+namespace WpfApp1.UserControls
+{
+    public partial class UserCard : UserControl
+    {
+        public UserCard()
+        {
+            InitializeComponent();
+        }
+        public string HumanName
+        {
+            get { return (string)GetValue(HumanNameProperty); }
+            set { SetValue(HumanNameProperty, value); }
+        }
+
+        public static readonly DependencyProperty HumanNameProperty = DependencyProperty.Register("HumanName", typeof(string), typeof(UserCard));
+        
+        public string Title
+        {
+            get { return (string)GetValue(TitleProperty); }
+            set { SetValue(TitleProperty, value); }
+        }
+
+        public static readonly DependencyProperty TitleProperty = DependencyProperty.Register("Title", typeof(string), typeof(UserCard));
+
+        public string Time
+        {
+            get { return (string)GetValue(TimeProperty); }
+            set { SetValue(TimeProperty, value); }
+        }
+
+        public static readonly DependencyProperty TimeProperty = DependencyProperty.Register("Time", typeof(string), typeof(UserCard));
+
+        public ImageSource Image
+        {
+            get { return (ImageSource)GetValue(ImageProperty); }
+            set { SetValue(ImageProperty, value); }
+        }
+
+        public static readonly DependencyProperty ImageProperty = DependencyProperty.Register("Image", typeof(ImageSource), typeof(UserCard));
+
+        public bool IsActive
+        {
+            get { return (bool)GetValue(IsActivProperty); }
+            set { SetValue(IsActivProperty, value); }
+        }
+
+        public static readonly DependencyProperty IsActivProperty = DependencyProperty.Register("FullName", typeof(bool), typeof(UserCard));
+        
+    }
+}
